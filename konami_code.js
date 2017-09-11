5e23e11f17409f79e9e9509294901f5bcbb99431
jasmine Magdi <jesojolay@yahoo.com>
2017-09-11 14:55:18 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,22 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
-  var body = document.querySlecetor("body");
-  body.addeventlistener("keydown", function(e){
-      const key = parseInt(e.which);
+    // Write your JavaScript code inside the init() function
+    var body = document.querySlecetor("body");
+    body.addeventlistener("keydown", function(e) {
+        const key = parseInt(e.detail || e.which);
 
-if (key === code[index]) {
-  index++;
+        if (key === code[index]) {
+            index++;
 
-  if (index === code.length) {
-    alert("Hurray!");
+            if (index === code.length) {
+                alert("Hurray!");
 
-    index = 0;
-  }
-} else {
-  index = 0;
-}
-  });
+                index = 0;
+            }
+        } else {
+            index = 0;
+        }
+    });
 }
 init();
