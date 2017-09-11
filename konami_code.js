ddccdf28dada25862bf8f0213a31e143ca60686c
jasmine Magdi <jesojolay@yahoo.com>
2017-09-11 14:59:07 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,5 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+var index = 0;
 function init() {
     // Write your JavaScript code inside the init() function
     var body = document.querySelector("body");
